197e577facc9fc966dd312b9d10a8e42581c260d
Michael Chu <mchchu88@gmail.com>
2019-09-02 15:18:38 -0400

    Initial stub for order_size.py

--- api/order_size.py
+++ api/order_size.py
@@ -2,3 +2,45 @@ class OrderSize:
 
     def init(self):
         pass
+
+    def debit(self, target_debit):
+        pass
+
+    def debit_func(self, func):
+        pass
+
+    def cost(self, target_cost):
+        pass
+
+    def cost_func(self, func):
+        pass
+
+    def credit(self, target_credit):
+        pass
+
+    def credit_func(self, func):
+        pass
+
+    def delta(self, target_delta):
+        pass
+
+    def delta_func(self, func):
+        pass
+
+    def margin(self, target_margin):
+        pass
+
+    def margin_func(self, func):
+        pass
+
+    def quantity(self, target_quantity):
+        pass
+
+    def quantity_func(self, func):
+        pass
+
+    def risk(self, target_risk):
+        pass
+
+    def risk_func(self, func):
+        pass
